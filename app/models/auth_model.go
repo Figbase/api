c7f59a4e26820aa8e3e497e5ad025decc94d4cac
ZyroGatsby <63269088+ZyroGatsby@users.noreply.github.com>
2024-02-07 00:21:25 +0100

    feat: update user model :sparkles:

--- app/models/auth_model.go
+++ app/models/auth_model.go
@@ -5,8 +5,10 @@ import "gorm.io/gorm"
 // SignUp struct to describe register a new user.
 type SignUp struct {
 	gorm.Model
-	Email    string `json:"email" validate:"required,email,lte=255"`
-	Password string `json:"password" validate:"required,lte=255"`
+	FirstName string `json:"firstname" validate:"required,lte=255"`
+	LastName  string `json:"lastname" validate:"required,lte=255"`
+	Email     string `json:"email" validate:"required,email,lte=255"`
+	Password  string `json:"password" validate:"required,lte=255"`
 	// UserRole string `json:"user_role" validate:"required,lte=25"`
 }
 
--- app/models/user_model.go
+++ app/models/user_model.go
@@ -14,6 +14,8 @@ type User struct {
 	ID           uuid.UUID `db:"id" json:"id" validate:"required,uuid"`
 	CreatedAt    time.Time `db:"created_at" json:"created_at"`
 	UpdatedAt    time.Time `db:"updated_at" json:"updated_at"`
+	FirstName    string    `db:"firstname" json:"firstname" validate:"required,lte=255"`
+	LastName     string    `db:"lastname" json:"lastname" validate:"required,lte=255"`
 	Email        string    `db:"email" json:"email" validate:"required,email,lte=255"`
 	PasswordHash string    `db:"password_hash" json:"password_hash,omitempty" validate:"required,lte=255"`
 	UserStatus   int       `db:"user_status" json:"user_status" validate:"required,len=1"`
